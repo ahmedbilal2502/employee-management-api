d018c2901be1ba7ff97aa94cc7b03f15fea7b0dd
Ahmed Bilal <ahmedbilal2502@gmail.com>
2024-10-10 11:08:20 +0500

    Cross Origin add global and controller level

--- src/main/java/com/employeemanagement/config/cors/CustomCorsConfiguration.java
+++ src/main/java/com/employeemanagement/config/cors/CustomCorsConfiguration.java
@@ -13,7 +13,7 @@ public class CustomCorsConfiguration implements CorsConfigurationSource {
     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("*"));
+        config.setAllowedOriginPatterns(List.of("https://*","http://*"));
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
 
