80a50ebc6972099779363c1fe8761b60680171fa
Ahmed Bilal <ahmedbilal2502@gmail.com>
2024-10-10 08:43:59 +0500

    Add Radis and CHeck

---
+++ src/main/java/com/employeemanagement/config/cache/RedisService.java
@@ -0,0 +1,21 @@
+package com.employeemanagement.config.cache;
+
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class RedisService {
+
+    private final RedisTemplate<String, Object> redisTemplate;
+
+    public void saveValue(String key, Object value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    public Object getValue(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+}
