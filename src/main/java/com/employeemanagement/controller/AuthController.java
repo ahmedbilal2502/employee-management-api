47108108d070c81099bd9267476a3d1bf1345d40
Ahmed Bilal <ahmedbilal2502@gmail.com>
2024-10-10 11:12:14 +0500

    Cross Origin add global and controller level

--- src/main/java/com/employeemanagement/controller/AuthController.java
+++ src/main/java/com/employeemanagement/controller/AuthController.java
@@ -18,6 +18,7 @@ import java.io.IOException;
 @RequestMapping(ConstantUrl.AUTH_BASE_URL)
 @AllArgsConstructor
 @Hidden
+@CrossOrigin
 public class AuthController {
 
     private final AuthService authService;
